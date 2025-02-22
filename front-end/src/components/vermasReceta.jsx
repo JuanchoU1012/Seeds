b0daa51503e12150887ffcca87f12e0223a7a225
JuanchoU1012 <jfusme@gmail.com>
2025-02-21 20:14:52 -0500

    'updaterecetas'

--- front-end/src/components/vermasReceta.jsx
+++ front-end/src/components/vermasReceta.jsx
@@ -1,43 +1,40 @@
-import { useState } from 'react';
-import '../estilos/recipemodal.css'
+import '../estilos/vermasmodal.css';
 import PropTypes from 'prop-types';
-const VermasReceta = ({isOpen, onClose, data}) => {
-    
+
+const VermasReceta = ({ isOpen, onClose, data }) => {
     if (!isOpen) return null;
-    
+
     const pasos = data.Pasos.split('|').map(paso => paso.split(": ")[1]);
-    
-    const [dataPasos, setData] = useState(pasos);
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setData({ ...data, [name]: value });
-    };
-
-    console.log(pasos)
-
 
     return (
         <div className="modalReceta" onClick={onClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
                 <button className="close-modal" onClick={onClose}>X</button>
-                <h2>Ver Mas Receta</h2>
-                <h2>Nombre Receta</h2>
-                {data['Nombre']}
-                <h2>Descripcion Receta</h2>
-                {data['Descripcion']}
-                <h2>Semillas Usadas</h2>
-                {data['Semillasusadas']}
-                <h2>Ingredeintes Usuados</h2>
-                {data['ProductosAdicionales']}
-                <h2>Pasos</h2>
-                <form action="" className="modal-form">
-                {pasos.map((paso, index) => (
-                    <input type="text" className="input-modal" value={paso} key={index} onChange={handleChange} />
-                ))}
-                </form>
-                <h2>Video</h2>
-
+                <header>
+                    <h2>Ver Más Receta</h2>
+                    <h3>Nombre Receta</h3>
+                    <p>{data['Nombre']}</p>
+                </header>
+                <section>
+                    <h3>Descripción Receta</h3>
+                    <p>{data['Descripcion']}</p>
+                    <h3>Semillas Usadas</h3>
+                    <p>{data['Semillasusadas']}</p>
+                    <h3>Ingredientes Usados</h3>
+                    <p>{data['ProductosAdicionales']}</p>
+                    <h3>Pasos</h3>
+                    <ol>
+                        {pasos.map((paso, index) => (
+                            <li key={index}>{paso}</li>
+                        ))}
+                    </ol>
+                </section>
+                <section>
+                    <h3>Video</h3>
+                    <div className="preview-container">
+                        <video src={`http://localhost:5000${data.videourl}`} alt="Preview" width="200" controls />
+                    </div>
+                </section>
             </div>
         </div>
     );
@@ -47,6 +44,6 @@ VermasReceta.propTypes = {
     isOpen: PropTypes.bool.isRequired,
     onClose: PropTypes.func.isRequired,
     data: PropTypes.object
-}
+};
 
 export default VermasReceta;
--- front-end/src/rutas/admin/usuariosAdmin.jsx
+++ front-end/src/rutas/admin/usuariosAdmin.jsx
@@ -7,22 +7,40 @@ import MenuLateral from '../../components/sidebarAdmin';
 import NavAdmin from '../../components/navegacionAdmin';
 import UserModal from '../../components/UserModal';
 
+import { getUserInfo } from '../../../helpers/getuserinfo';
+import { getTokenInfo } from '../../../helpers/getjwt';
+import { U401 } from '../../components/401';
+
 const API = import.meta.env.VITE_REACT_APP_API || 'http://localhost:5000';
 
 export const UsuariosAdmin = () => {
+
+    const [token, setToken] = useState(null);
+    const [userData, setUserData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
     const [FormData, setFormData] = useState({
         email: "",
         password: "",
         rol: ""
     });
-    const [dataUsuarios, setDataUsuarios] = useState([]);
     const [selectedUsuario, setSelectedUsuario] = useState(null);
     const [showEditarModal, setShowEditarModal] = useState(false);
     const [showNuevoModal, setShowNuevoModal] = useState(false);
-    
+
+    const [dataUsuarios, setDataUsuarios] = useState([]);
     const [filteredUsuarios, setFilteredUsuarios] = useState([])
     const [searchTerm, setSearchTerm] = useState("")
 
+    useEffect(()=>{
+        const fetchdata = async () =>{
+            const userdata = await getUserInfo();
+            const token = await getTokenInfo();
+            setUserData(userdata);
+            setToken(token);
+            setIsLoading(false);
+        }
+        fetchdata();
+    }, [])
 
     const handleSearch = (e) => {
         const value = e.target.value.toLowerCase();
@@ -34,7 +52,7 @@ export const UsuariosAdmin = () => {
     const validateFormData = () => {
         return FormData.email && FormData.password && FormData.rol;
     };
-    
+
 
     const handleNuevoUsuario = async (e) => {
         e.preventDefault();
@@ -68,9 +86,9 @@ export const UsuariosAdmin = () => {
     };
 
     useEffect(() => {
-            const fetchData = async () => {
+        const fetchData = async () => {
             // console.log("Fetching seed data...");
-            try{
+            try {
                 const response = await fetch(`${API}/users/get`, {
                     method: 'GET',
                     credentials: 'include',
@@ -84,7 +102,7 @@ export const UsuariosAdmin = () => {
                     setDataUsuarios(data);
                     setFilteredUsuarios(data);
                 }
-                else{
+                else {
                     const data = await response.json();
                     console.error("Failed to fetch user data:", data);
                 }
@@ -126,6 +144,14 @@ export const UsuariosAdmin = () => {
         setSelectedUsuario(usuario);
         setShowEditarModal(true);
     };
+    
+    if (isLoading) {
+        return <div className="text-center mt-5">Loading...</div>;
+    }
+
+    if (!userData || userData.rol !== 0) {
+        return <U401 />;
+    }
 
     return (
         <div className="UsuariosAdmin">
@@ -133,11 +159,11 @@ export const UsuariosAdmin = () => {
             <MenuLateral />
             <h1>Usuarios</h1>
             <div className="search-container">
-                <input className="input-search" 
-                type="text" 
-                placeholder="Buscar..." 
-                value={searchTerm} 
-                onChange={handleSearch}/>
+                <input className="input-search"
+                    type="text"
+                    placeholder="Buscar..."
+                    value={searchTerm}
+                    onChange={handleSearch} />
             </div>
             <button className="botonNuevaRecetaAdmin" onClick={() => setShowNuevoModal(true)}>Nueva Receta</button>
             <table className="crudUsuariosAdmin">
@@ -150,11 +176,24 @@ export const UsuariosAdmin = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {filteredUsuarios.map((user) => (
+                    {(!filteredUsuarios)?
+                    <tr>
+                        <td colSpan="5">No hay usuarios</td>
+                    </tr> 
+                    : 
+                    filteredUsuarios.map((user) => (
                         <tr key={user.IdAccesoUsuario}>
                             <td>{user.IdAccesoUsuario}</td>
                             <td>{user.Email}</td>
-                            <td>{user.Rol}</td>
+                            <td>
+                                {user.Rol === 0 ? (
+                                    <span>Administrador</span>
+                                ) : user.Rol === 1 ? (
+                                    <span>Vendedor</span>
+                                ) : (
+                                    <span>Cleinte</span>
+                                )}
+                            </td>
                             <td className="accionesUsuariosAdmin">
                                 <NavLink className='actulizarUsuarios' onClick={() => handleEditar(user)}>
                                     <FontAwesomeIcon icon={faPencil} style={{ color: "#000000" }} />
@@ -166,21 +205,21 @@ export const UsuariosAdmin = () => {
             </table>
 
             {/* Modal para "Editar" */}
-            <UserModal 
-                isOpen={showEditarModal} 
-                onClose={() => setShowEditarModal(false)} 
-                onSubmit={handleUpdate} 
-                data={selectedUsuario} 
-                setData={setSelectedUsuario} 
+            <UserModal
+                isOpen={showEditarModal}
+                onClose={() => setShowEditarModal(false)}
+                onSubmit={handleUpdate}
+                data={selectedUsuario}
+                setData={setSelectedUsuario}
             />
 
             {/* Modal para nuevo usuario */}
-            <UserModal 
-                isOpen={showNuevoModal} 
-                onClose={() => setShowNuevoModal(false)} 
-                onSubmit={handleNuevoUsuario} 
-                data={FormData} 
-                setData={setFormData} 
+            <UserModal
+                isOpen={showNuevoModal}
+                onClose={() => setShowNuevoModal(false)}
+                onSubmit={handleNuevoUsuario}
+                data={FormData}
+                setData={setFormData}
             />
         </div>
     );
